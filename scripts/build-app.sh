eceefcace2b9f3a045c1e7f668f91aa719d2be67
Anirudh Sriram <anirudh@Anirudhs-Mac-mini.local>
2026-07-30 10:52:30 +0530

    refactor: normalize resources directory casing

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -16,8 +16,8 @@ binary_dir=$(swift build --disable-sandbox -c "$configuration" --show-bin-path)
 
 mkdir -p "$app_dir/Contents/MacOS" "$app_dir/Contents/Resources"
 cp "$binary_dir/RemoteBridge" "$app_dir/Contents/MacOS/RemoteBridge"
-cp "$repo_dir/resources/Info.plist" "$app_dir/Contents/Info.plist"
-cp "$repo_dir/resources/AppIcon.icns" "$app_dir/Contents/Resources/AppIcon.icns"
+cp "$repo_dir/Resources/Info.plist" "$app_dir/Contents/Info.plist"
+cp "$repo_dir/Resources/AppIcon.icns" "$app_dir/Contents/Resources/AppIcon.icns"
 xattr -cr "$app_dir"
 codesign --force --deep --sign - "$app_dir"
 
